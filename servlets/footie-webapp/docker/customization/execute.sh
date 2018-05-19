9aaae3886323e6d5fcc3de109bbabde009a00ecf
kinya <kiernan920@gmail.com>
2018-05-19 23:02:16 +0100

    added slave and master

--- servlets/footie-webapp/docker/customization/execute.sh
+++ servlets/footie-webapp/docker/customization/execute.sh
@@ -8,6 +8,7 @@
 JBOSS_HOME=/opt/jboss/wildfly
 JBOSS_CLI=$JBOSS_HOME/bin/jboss-cli.sh
 JBOSS_MODE=${1:-"standalone"}
+JBOSS_MODE_ARRAY=( "standalone" "domain" )
 JBOSS_CONFIG=${2:-"$JBOSS_MODE.xml"}
 
 function wait_for_server() {
@@ -15,23 +16,26 @@ function wait_for_server() {
     sleep 1
   done
 }
-
+cp -rf /opt/jboss/wildfly/ha/host.xml /opt/jboss/wildfly/domain/configuration
 cp -rf /opt/jboss/wildfly/customization/standalone.conf /opt/jboss/wildfly/bin/
 
 echo "=> Starting WildFly server"
-$JBOSS_HOME/bin/$JBOSS_MODE.sh -c $JBOSS_CONFIG > /dev/null &
+$JBOSS_HOME/bin/${JBOSS_MODE_ARRAY[1]}.sh > /dev/null &
 
-echo "=> Waiting for the server to boot"
-wait_for_server
+#echo "=> Waiting for the server to boot"
+#wait_for_server
 
-echo "=> Executing the commands"
-$JBOSS_CLI -c --file=`dirname "$0"`/commands.cli
+#echo "=> Executing the commands"
+#$JBOSS_CLI -c --file=`dirname "$0"`/commands.cli
 
-echo "=> Copying WAR"
+#echo "=> Copying WAR"
 
-cp -r /opt/jboss/wildfly/warToBeDeployed/footie-webapp.war /opt/jboss/wildfly/standalone/deployments/
+#cp -r /opt/jboss/wildfly/warToBeDeployed/footie-webapp.war /opt/jboss/wildfly/standalone/deployments/
 
-echo "=> Tailing server.log"
+#echo "=> Tailing server.log"
 
-tail -f /opt/jboss/wildfly/standalone/log/server.log
-
+#tail -f /opt/jboss/wildfly/standalone/log/server.log
+sleep 5
+/opt/jboss/wildfly/bin/add-user.sh --silent=true admin1 admin1
+/opt/jboss/wildfly/bin/add-user.sh --silent=true slave slave
+tail -f /opt/jboss/wildfly/domain/log/host-controller.log
--- servlets/footie-webapp/docker/customization/execute2.sh
+++ servlets/footie-webapp/docker/customization/execute2.sh
@@ -8,6 +8,7 @@
 JBOSS_HOME=/opt/jboss/wildfly
 JBOSS_CLI=$JBOSS_HOME/bin/jboss-cli.sh
 JBOSS_MODE=${1:-"standalone"}
+JBOSS_MODE_ARRAY=( "standalone" "domain" )
 JBOSS_CONFIG=${2:-"$JBOSS_MODE.xml"}
 
 function wait_for_server() {
@@ -15,23 +16,27 @@ function wait_for_server() {
     sleep 5
   done
 }
+cp -rf /opt/jboss/wildfly/customization/host.xml /opt/jboss/wildfly/domain/configuration
 cp -rf /opt/jboss/wildfly/customization/standalone.xml /opt/jboss/wildfly/standalone/configuration/
 cp -rf /opt/jboss/wildfly/customization/standalone.conf /opt/jboss/wildfly/bin/
 
 echo "=> Starting WildFly server"
-$JBOSS_HOME/bin/$JBOSS_MODE.sh -c $JBOSS_CONFIG > /dev/null &
+sleep 7
+$JBOSS_HOME/bin/${JBOSS_MODE_ARRAY[1]}.sh > /dev/null &
+#-c $JBOSS_CONFIG
 
-echo "=> Waiting for the server to boot"
-wait_for_server
+#echo "=> Waiting for the server to boot"
+#wait_for_server
 
-echo "=> Executing the commands"
-$JBOSS_CLI -c --controller=localhost:9992 --file=`dirname "$0"`/commands.cli
+#echo "=> Executing the commands"
+#$JBOSS_CLI -c --controller=localhost:9992 --file=`dirname "$0"`/commands.cli
 
-echo "=> Copying WAR"
+#echo "=> Copying WAR"
 
-cp -r /opt/jboss/wildfly/warToBeDeployed/footie-webapp.war /opt/jboss/wildfly/standalone/deployments/
+#cp -r /opt/jboss/wildfly/warToBeDeployed/footie-webapp.war /opt/jboss/wildfly/standalone/deployments/
 
-echo "=> Tailing server.log"
+#echo "=> Tailing server.log"
 
-tail -f /opt/jboss/wildfly/standalone/log/server.log
-
+#tail -f /opt/jboss/wildfly/standalone/log/server.log
+sleep 5
+tail -f /opt/jboss/wildfly/domain/log/host-controller.log
